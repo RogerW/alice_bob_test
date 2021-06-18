fe8b1c09a94796b8c25360804d8607936d06bfa7
Vorobyev Stanislav <stsvorobev@gmail.com>
2021-06-18 11:51:36 +0500

    Initial

---
+++ alice/test-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+echo "Environment: $RAILS_ENV"
+
+# Remove a potentially pre-existing server.pid for Rails.
+if [ -f /var/app/tmp/pids/server.pid ]; then
+  rm /var/app/tmp/pids/server.pid
+fi
+
+bundle check || bundle install --jobs 10 --retry 5
+
+rails db:environment:set RAILS_ENV=test
+
+bundle exec ${@}
